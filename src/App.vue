a51b8ca88c31ba0e85d4051c5d34ad74879fcdcc
guakun01 <guakun01@gmail.com>
2019-04-24 08:20:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <Second :second="second" />
+    <Minute :minute="minute" />
+    <Hour :hour="hour" />
+    <Apm :apm="apm" />
+    <Week :week="week" />
+    <Day :day="day" />
+    <Month :month="month" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+.home {
+  ul {
+    list-style-type: none;
+    padding: 0;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    height: 60px;
+    width: 60px;
+    li {
+      position: absolute;
+      height: 60px;
+      width: 60px;
+      color: #fff;
+      text-align: center;
+      font-size: 14px;
+      line-height: 20px;
+    }
+  }
+}
+
+</style>

0b340aa9bae8770678cad35a4e380f8b5e5508f3
guakun01 <guakun01@gmail.com>
2019-04-24 08:24:54 +0800

    新建 second 组件

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,19 @@
 <template>
   <div class="home">
-    <Second :second="second" />
-    <Minute :minute="minute" />
-    <Hour :hour="hour" />
-    <Apm :apm="apm" />
-    <Week :week="week" />
-    <Day :day="day" />
-    <Month :month="month" />
+    <gua-second :second="second" />
   </div>
 </template>
 
 <script>
+import GuaSecond from '@/components/second'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    GuaSecond,
+  }
 }
 </script>
 
 <style lang="scss">
-.home {
-  ul {
-    list-style-type: none;
-    padding: 0;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-    height: 60px;
-    width: 60px;
-    li {
-      position: absolute;
-      height: 60px;
-      width: 60px;
-      color: #fff;
-      text-align: center;
-      font-size: 14px;
-      line-height: 20px;
-    }
-  }
-}
 
 </style>

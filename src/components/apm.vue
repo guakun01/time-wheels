90c06690f665c78f1f6861ec86f2db78298e8041
guakun01 <guakun01@gmail.com>
2019-04-24 14:34:31 +0800

    添加所有组件 完成 apm 组件

---
+++ src/components/apm.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul :style="ulStyles">
+    <li v-for="(item, index) in liList" :key="`apm-${index}`" :class="{hover: index===apm-1}">
+      {{item}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'GuaApm',
+  props: ['apm'],
+  computed: {
+    liList() {
+      return this.$t('apm')
+    },
+    ulStyles() {
+      return {
+        transform: `rotate(${-(this.apm * 60)}deg)`
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  ul {
+    z-index: 5;
+    @for $i from 1 through 2 {
+      li:nth-child(#{$i}) {
+        transform: rotate(#{$i * 60deg}) translateX(230px);
+      }
+    }
+  }
+</style>
